19256c1dbe6c41b0e39cf6359d3a0f843c6ca538
OlivierPMaas <32394843+OlivierPMaas@users.noreply.github.com>
2019-11-07 15:49:30 +0100

    Added evaluator methods scaffolding

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import HousingMarket.Household.PersonalityType;
 import HousingMarket.Household.HouseholdType;
 import HousingMarket.HousingMarket;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args){
@@ -15,11 +17,14 @@ public class Main {
             MatchingEvaluator matchingEvaluator= new MatchingEvaluator(matching);
             matchingEvaluator.evaluateHouseholdlessHouses();
             matchingEvaluator.evaluateHouselessHouseholds();
+            matchingEvaluator.evaluateAccessibilityFit();
         } catch (MatchingEvaluator.InvalidMatchingException
                 | HousingMarket.FreeSpaceException
                 | Household.InvalidHouseholdException
                 | Matching.HouseholdAlreadyMatchedException
-                | Matching.HouseAlreadyMatchedException e) {
+                | Matching.HouseAlreadyMatchedException
+                | Matching.HouseholdLinkedToMultipleException
+                | Matching.HouseholdLinkedToHouseholdException e) {
             System.err.println(e.getMessage());
         }
     }
@@ -31,15 +36,24 @@ public class Main {
 
         House house1 = new House(false, 500,3,50, false, 2011);
         House house2 = new House(false, 600, 4, 60, true, 2016);
+        House house3 = new House(false, 600, 4, 60, false, 2016);
         Household household1 = new Household(43, 25000, HouseholdType.HH2, 4, 2, LabelType.DOORSTROMER , PersonalityType.RED, 12 * 8, false);
         Household household2 = new Household(20, 20000, HouseholdType.ONE, 1, 0, LabelType.STARTER, PersonalityType.GREEN, 12 * 1, false);
         Household household3 = new Household(36, 35000, HouseholdType.TWO, 2, 0, LabelType.DOORSTROMER, PersonalityType.BLUE, 12 * 4, true);
+        Household household4 = new Household(70, 25000, HouseholdType.HH2, 2, 0, LabelType.DOORSTROMER, PersonalityType.GREEN, 12*6, true);
+        Household household5 = new Household(70, 25000, HouseholdType.HH2, 2, 0, LabelType.DOORSTROMER, PersonalityType.GREEN, 12*6, true);
+
         matching.addHouse(house1);
         matching.addHouse(house2);
+        matching.addHouse(house3);
         matching.addHousehold(household1);
         matching.addHousehold(household2);
         matching.addHousehold(household3);
+        matching.addHousehold(household4);
+        matching.addHousehold(household5);
         matching.connect(house1, household1);
+        matching.connect(house2, household4);
+        matching.connect(house3, household5);
 
     }
 
--- src/MatchingEvaluator.java
+++ src/MatchingEvaluator.java
@@ -1,13 +1,17 @@
 import HousingMarket.House.House;
 import HousingMarket.Household.Household;
 
+import java.util.HashSet;
+
 public class MatchingEvaluator {
 
     private Matching matching;
+
 //    Takes Matching and evaluates based on several criteria.
     public MatchingEvaluator(Matching matching) {
         this.matching = matching;
     }
+
 
     public float evaluateHouselessHouseholds() throws InvalidMatchingException {
         float householdsCount = this.matching.getHouseholds().size();
@@ -66,8 +70,31 @@ public class MatchingEvaluator {
         // TODO: finish; rooms and meters.
     }
 
-    public void evaluateAccessibilityFit() {
-        // TODO: finish
+    public float evaluateAccessibilityFit()
+            throws Matching.HouseholdLinkedToHouseholdException,
+            Matching.HouseholdLinkedToMultipleException {
+        float householdsAbove65WithHouses = 0;
+        float householdsAbove65WithHousesAndAccessibility = 0;
+        for (Household household : this.matching.getHouseholds()
+             ) {
+                if (household.getAge() >= 65) {
+                    House house = matching.getHouseFromHousehold(household);
+                    if (house != null) {
+                        householdsAbove65WithHouses++;
+                        if (house.getAccessibility()) {
+                            householdsAbove65WithHousesAndAccessibility++;
+                        }
+                    }
+                }
+        }
+        float result = 0;
+        if (householdsAbove65WithHouses > 0) {
+            result = householdsAbove65WithHousesAndAccessibility / householdsAbove65WithHouses;
+            System.out.println(result * 100 + "% of households above the age of 65 that own houses, own houses that are accessible.");
+        } else {
+            System.out.println("Matching does not contain households above the age of 65 that own houses.");
+        }
+        return result;
     }
 
     public void evaluateDistributionOverHouseSize() {
@@ -102,6 +129,9 @@ public class MatchingEvaluator {
         // TODO: finish
     }
 
+    public void evaluateTotal() {
+        // TODO: finish last.
+    }
 
     public class InvalidMatchingException extends Exception {
         public InvalidMatchingException(String errorMessage) {
