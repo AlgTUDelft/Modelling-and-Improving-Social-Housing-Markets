18791b84f1e04a8c567b6562446f889b95b90675
OlivierPMaas <32394843+OlivierPMaas@users.noreply.github.com>
2019-12-04 16:17:02 +0100

    Started work on algorithm.

---
+++ src/Algorithms/MinCostPerfectMatchingAlgorithm.java
@@ -0,0 +1,16 @@
+package Algorithms;
+
+import Matching.Matching;
+
+public class MinCostPerfectMatchingAlgorithm {
+    // Adapted from "Algorithm Design" by Jon Kleinberg and Eva Tardos.
+    private Matching matching;
+
+    public MinCostPerfectMatchingAlgorithm(Matching matching) {
+        this.matching = matching;
+    }
+
+    public float FindMinCostPerfectMatching() {
+        return (float) 0.0;
+    }
+}
